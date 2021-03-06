fc03bc4da37603b96f5847f28590d1cc01874248
Alamin Mirdha <mdalaminmirdha1211@gmail.com>
2021-03-07 03:27:29 +0600

    News site develop by material ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
-
+import News from './Components/News/News';
 function App() {
+  const [articles, setArticles]=useState([]);
+  useEffect(()=>{
+    const url='http://newsapi.org/v2/everything?q=tesla&from=2021-02-06&sortBy=publishedAt&apiKey=ec8c19aa2b964399a9e77e38cae7e73f'
+    fetch(url)
+    .then (res => res .json())
+    .then (data=> setArticles(data.articles))
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {
+        articles.map(article=><News article={article}></News>)
+      }
     </div>
   );
 }
